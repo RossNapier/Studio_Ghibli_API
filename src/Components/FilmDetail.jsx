12e619fd9ee1b84fcd317295b7cc01313ab7e46b
Ross Napier <ross-napier@hotmail.com>
2021-11-27 15:42:26 +0000

    film details added

---
+++ src/Components/FilmDetail.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const FilmDetail = ({film, onBannerClick}) => {
+
+    const handleClick = function() {
+        onBannerClick();
+    }
+
+    return(
+        <div id="film-detail" onClick = {handleClick}>
+            <section id = "film-desc">
+                <p id="film-detail-title">{film.title} • {film.original_title} • {film.release_date}</p>
+                <p>{film.description}</p>
+            </section>
+            <div id = "rt-score">
+                RT SCORE<p class = "big-text">{film.rt_score}</p>
+            </div>
+            <div id = "run-time">
+                RUN TIME<p class = "big-text">{film.running_time}</p>
+            </div>
+        </div>
+    )
+}
+
+export default FilmDetail;
